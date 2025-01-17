9ca7ffba808bbb4afd983cab51f393dccfe9baab
vaultspeed <info@vaultspeed.com>
2025-01-17 09:18:52 +0200

    Vaultspeed ETL generation (7)
    DV_NAME: moto_scn01 - Release: R1(1) - Comment: VaultSpeed setup automation - Release date: 2025/01/16 14:54:27; 
    BV release: release1(2) - Comment: VaultSpeed Automation - Release date: 2025/01/16 14:56:23

--- DDL/INCR/STAGING_LAYER/312_001_stg_ms_products_incr.sql
+++ DDL/INCR/STAGING_LAYER/312_001_stg_ms_products_incr.sql
@@ -11,7 +11,7 @@ AS $function$
     \_/ \__,_|\__,_|_|\__|___/ .__/ \___|\___|\__,_|     /_/ \/_/\__/       
                              |_|                                            
 
-Vaultspeed version: 5.7.2.16, generation date: 2025/01/16 15:00:22
+Vaultspeed version: 5.7.2.16, generation date: 2025/01/17 07:04:17
 DV_NAME: moto_scn01 - Release: R1(1) - Comment: VaultSpeed setup automation - Release date: 2025/01/16 14:54:27, 
 BV release: release1(2) - Comment: VaultSpeed Automation - Release date: 2025/01/16 14:56:23, 
 SRC_NAME: moto_sales_scn01 - Release: moto_sales_scn01(1) - Comment: VaultSpeed automated setup - Release date: 2025/01/16 14:51:08
